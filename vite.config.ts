6f115ac81b28a89c825fad7eb43d6d727c504540
anatolybir <anatolb07@gmail.com>
2025-02-06 17:10:21 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react()],
-    resolve: {
-        alias: {
-            '@': '/src',
-        },
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      '@': '/src',
     },
+  },
 })
